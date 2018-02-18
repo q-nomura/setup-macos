f72e34436d53686fe2ee4e2a8387630603ea1fe4
Shuhei Nomura <info@danmaq.com>
2018-02-19 05:40:53 +0900

    Added that installation for xcode CLI tools.

--- install-ansible.sh
+++ install-ansible.sh
@@ -5,6 +5,7 @@ echo 'Start setup...'
 if [ `which brew | wc -l` -eq 0 ]
 then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    xcode-select --install
 fi
 
 # Install Python and Ansible
